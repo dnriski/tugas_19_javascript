7eade4e01676b8f83905a66c381cad010563804f
dnriski <nugrohoriski159@gmail.com>
2023-02-09 18:42:11 +0700

    first commit

---
+++ tugas19.js
@@ -0,0 +1,19 @@
+const Repeat = () => {
+  console.log("Semangat...".repeat(5));
+};
+
+Repeat();
+
+const Repeat2 = () => {
+  console.log("*".repeat(5));
+  console.log("*".repeat(20));
+  console.log("*".repeat(40));
+  console.log("*".repeat(60));
+  console.log("*".repeat(80));
+  console.log("*".repeat(60));
+  console.log("*".repeat(40));
+  console.log("*".repeat(20));
+  console.log("*".repeat(5));
+};
+
+Repeat2();
